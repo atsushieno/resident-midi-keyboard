53a1628e2e3801b473bdc67e164638b4ff4e9fee
Atsushi Eno <atsushieno@gmail.com>
2023-08-04 19:17:05 +0800

    bump some dependency versions.

--- app/src/main/java/org/androidaudioplugin/residentmidikeyboard/MainActivity.kt
+++ app/src/main/java/org/androidaudioplugin/residentmidikeyboard/MainActivity.kt
@@ -26,7 +26,6 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.ComposeCompilerApi
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
